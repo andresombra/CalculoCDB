2f7ac651b958eb519b87c0c2b674ac5d8469c0f2
andresombra <andresombra.net@gmail.com>
2024-07-25 15:03:54 -0300

    Incluido Domain

---
+++ SolutionCDB/SolutionCDB.Domain/DTO/RequestInvestimento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionCDB.Domain.DTO
+{
+    public class RequestInvestimento
+    {
+        public decimal Valor { get; set; }
+        public int PrazoMes { get; set; }
+    }
+}
---
+++ SolutionCDB/SolutionCDB.Domain/DTO/ResponseInvestimento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionCDB.Domain.DTO
+{
+    public class ResponseInvestimento
+    {
+        public double ValorBruto { get; set; }
+        public double ValorLiquido { get; set; }
+    }
+}
