a2f68aef42647a085f668a1e41d51a06f3a5b6a0
andresombra <andresombra.net@gmail.com>
2024-07-26 09:50:30 -0300

    Refatorando e funcionando ok.

--- SolutionCDB/SolutionCDB.Web/solutioncdb.web.client/src/app/app.module.ts
+++ SolutionCDB/SolutionCDB.Web/solutioncdb.web.client/src/app/app.module.ts
@@ -4,20 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CalculoInvestimentoComponent } from './investimentoCDB/investimentoCDB.component'; // Importe seu componente
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CalculoInvestimentoComponent // Adicione seu componente aqui
+    AppComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule, 
-    RouterModule.forRoot([
-      { path: '', component: CalculoInvestimentoComponent, pathMatch: 'full' } // Atualize para o seu componente
-    ])
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
