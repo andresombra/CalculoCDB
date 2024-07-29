c6f7af0f2a71327552ee9fe2aab6d54340413b95
andresombra <andresombra.net@gmail.com>
2024-07-29 15:15:01 -0300

    Alteracoes

---
+++ SolutionCDB/SolutionCDB.Tests/TestePrazoMeses.cs
@@ -0,0 +1,34 @@
+﻿using SolutionCDB.Domain.DTO;
+using SolutionCDB.Domain.Interfaces;
+using SolutionCDB.Service.Service;
+namespace SolutionCDB.Tests
+{
+    [TestFixture]
+    public class TestePrazoMeses
+    {
+        CdbService _cdbService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _cdbService = new CdbService();
+        }
+
+        [Test]
+        [TestCase(1000,6)]
+        [TestCase(1000,12)]
+        [TestCase(1000,24)]
+        [TestCase(1000,36)]
+        public void CalcularCdbDadosBasicos(double valorInvestimento, int prazoMes)
+        {
+
+            var request = new RequestInvestimento() { ValorInvestimento = valorInvestimento, PrazoMes = prazoMes };
+
+            var response = _cdbService.CalcularCdb(request).Result;
+
+            Assert.IsNotNull(response);
+
+        }
+
+    }
+}
